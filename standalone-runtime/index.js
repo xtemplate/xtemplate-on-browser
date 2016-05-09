cf42a1640c3be1ccb4c8a34203cf7b9178730082
yiminghe <yiminghe@gmail.com>
2016-05-09 21:01:57 +0800

    add loop demo

--- standalone-runtime/index.js
+++ standalone-runtime/index.js
@@ -1,5 +1,6 @@
 var Xtemplate = require('xtemplate/lib/runtime');
 document.getElementById('result').innerHTML = new Xtemplate(require('./xtpl/a.xtpl')).render({
   x: 1,
-  y: 2
+  y: 2,
+  arr: [1, 2, 3, 4, 5, 6, 7,12],
 });
